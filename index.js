384f4d11bf69dae946dd3dd43a2dbe3ac98197c9
Seunggyu <seunggyul14@icloud.com>
2022-11-28 19:55:04 -0800

    commit tree 483641f5d96c070fbc37fc3653bee78c4a7b9f3b

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield'];
+
+function destructivelyAppendCat(names) {
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat(names) {
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat(names) {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(names) {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const cats2 = [...cats, name];
+    return cats2
+}
+
+function prependCat(name) {
+    const cats2 = [name, ...cats];
+    return cats2
+}
+
+function removeLastCat(names) {
+    const cats2 = cats.slice(0, 2);
+    return cats2
+}
+
+function removeFirstCat(names) {
+ const cats2 = cats.slice(1);
+ return cats2
+}
